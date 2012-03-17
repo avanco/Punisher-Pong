ff5dccbabf4d6210ac68c5210b0bcf1d9c1bd26c
avanco <avanco89@gmail.com>
2012-03-17 00:35:46 -0300

    Now I have two players and a bug

--- pong.asm
+++ pong.asm
@@ -9,59 +9,89 @@
 ; Variables (memory region: $80 to $FF)
 YPosBall = $80
 BallSize = $81
+DirectionBall = $82
+Top = $83
+Down = $84
+BallLeftRight = $85
+Player0Pos = $86
+Player1Pos = $87
+Player0Size = $88
+Player1Size = $89
 
 Start			; init stuff:
-	SEI			; no interruptions
-	CLD			; clear BCD Math Bit
+	SEI		; no interruptions
+	CLD		; clear BCD Math Bit
 	LDX #$FF	; X to up
-	TXS			; stack pointer = X
+	TXS		; stack pointer = X
 	LDA #0
 
 ClearMem
 	STA 0,X		; MEM[X+0] = Accumulator value
-	DEX			; X--
+	DEX		; X--
 	BNE ClearMem
 
-	LDA #0
+	LDA #190
 	STA YPosBall
+	LDA #194
+	STA Top		; it will allow change ball direction to down
+	LDA #6
+	STA Down	; it will allow change ball direction to up
 	LDA #0
 	STA BallSize
+	STA DirectionBall	; 0: down, 1: up
+	LDA #$10
+	STA BallLeftRight	; $10: left, $F0: right
+	LDA #110
+	STA Player0Pos
+	STA Player1Pos
+	LDA #0
+	STA Player0Size
+	STA Player1Size
 
 SetColors
 	LDA #$44
 	STA COLUBK	; background color
-	LDA #$21		; defining PlayField size (D0) and Ball size (D4, D5)
+	LDA #$21	; defining PlayField size (D0) and Ball size (D4, D5)
 	STA CTRLPF
 	LDA #$10
 	STA PF0
+	LDA #$10	; ball color
+	STA COLUPF
 
 MainLoop
-	LDA	#2		; VSYNC D1 = 1 --> turn off electron beam,
-				;  position it at the top of the screen
+	LDA	#2	; VSYNC D1 = 1 --> turn off electron beam,
+			;  position it at the top of the screen
 	STA VSYNC	; VSYNC must be sent for at least 3 scanlines
 	STA WSYNC	;  by TIA
 	STA WSYNC
 	STA WSYNC
-	LDA #0		; -- CHECK IN-1
-	STA VSYNC	; the time is over -- CHECK IN-1
+	LDA #0
+	STA VSYNC	; the time is over
 
 	LDA #43		; 37 scanlines * 76 machine cycles = 2812
 				; 2812 + 5 cycles(init timer) + 3 cycles(STA WSYNC) + 6 cycles(loop check)
 				; Finally we have 2812-14=2798 cycles while VBLANK scanlines, and 2798/64=43
 	STA TIM64T
 
-	LDA #2		; -- CHECK IN-2
-	STA VBLANK	; -- CHECK IN-2
-	#LDA #0		; -- CHECK OUT-1
-	#STA VSYNC	; the time is over -- CHECK OUT-1
+	LDA #2
+	STA VBLANK
 
 	; game logic, timer is running
+	LDA DirectionBall
+	BNE BallUp
 	DEC YPosBall
-	LDA YPosBall
+	LDA Down
+	CMP YPosBall
+	BNE WaitVBlankEnd
+BallUp
+	LDA #1
+	STA DirectionBall
+	INC YPosBall
+	LDA Top
+	CMP YPosBall
 	BNE WaitVBlankEnd
 	LDA #0
-	STA YPosBall
-	
+	STA DirectionBall
 
 WaitVBlankEnd
 	LDA INTIM	; load timer
@@ -72,39 +102,75 @@ WaitVBlankEnd
 	STA WSYNC	; wait for scanline end, we do not wanna begin at the middle of one
 	STA VBLANK	; VBLANK D1 = 0
 
-	LDA #$F0	; speed
+	LDA BallLeftRight	; speed
 	STA HMBL
-	LDA #$10	; speed
-	STA HMM0
+	LDA #$00			; speed
+	STA GRP0			; D0 to D7 are considered
+	STA GRP1
 	STA WSYNC
 	STA HMOVE	; strobe register like WSYNC
 
 ScanLoop
 	STA WSYNC
-
-	CPY YPosBall		; check if we are at ball position, scanline
-	BEQ ActiveBallSize	;
-	LDA BallSize		;
-	BNE Drawing			;
-NoBall					;
-	LDA #0				;
-	STA ENABL			;
-	JMP Continue		;
-ActiveBallSize			;
-	LDA #8				;
-	STA BallSize		;
-Drawing					;
-	LDA #2				;
-	STA ENABL			;
-	DEC BallSize		;
-Continue
+; check if we are at ball position, scanline
+	CPY YPosBall
+	BEQ ActiveBallSize
+	LDA BallSize
+	BNE DrawingBall
+NoBall
+	LDA #0
+	STA ENABL
+	JMP OutBall
+ActiveBallSize
+	LDA #8
+	STA BallSize
+DrawingBall
 	LDA #2
-	STA ENAM0
+	STA ENABL
+	DEC BallSize
+OutBall
+; check Player0 position
+	CPY Player0Pos
+	BEQ ActivePlayer0Size
+	LDA Player0Size
+	BNE DrawingPlayer0
+NoPlayer0
+	LDA #0
+	STA GRP0
+	JMP OutPlayer0
+ActivePlayer0Size
+	LDA #30
+	STA Player0Size
+DrawingPlayer0
+	LDA #2
+	STA GRP0
+	DEC Player0Size
+OutPlayer0
+; check Player1 position
+	CPY Player1Pos
+	BEQ ActivePlayer1Size
+	LDA Player1Size
+	BNE DrawingPlayer1
+NoPlayer1
+	LDA #0
+	STA GRP1
+	JMP OutPlayer1
+ActivePlayer1Size
+	LDA #30
+	STA Player1Size
+DrawingPlayer1
+	LDA #2
+	STA GRP1
+	DEC Player1Size
+OutPlayer1
+
 ; check collision
 	LDA #%1000000
 	BIT CXM0FB
 	BEQ NoCollision
 	STY COLUBK	; background color
+	LDA #0
+	STA ENAM0
 	STA CXCLR
 NoCollision
 	DEY
